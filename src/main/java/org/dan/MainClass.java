4fc3bd658a596dfd4f287d118838632ada4094d1
Dan Ratiu <dan.ratiu24@gmail.com>
2021-02-07 17:49:54 +0100

    added other method

--- src/main/java/org/dan/MainClass.java
+++ src/main/java/org/dan/MainClass.java
@@ -3,8 +3,16 @@ package org.dan;
 public class MainClass {
 
 	public static void main(String[] args) {
-		System.out.println("Soem text");
+		System.out.println("Soem better text");
 
+	}
+	
+	public static void someMethod () {
+		System.out.println("someMethod was invocked");
+	}	
+	
+	public void otherMethod() {
+		System.out.println("otherMethod was invocked");
 	}
 
 }
